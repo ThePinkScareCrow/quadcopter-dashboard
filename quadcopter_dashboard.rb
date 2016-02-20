c667a1739520bbf057498e24317f64c2878c342e
Darshan Rai <darshanjrai@gmail.com>
2016-02-21 01:09:26 +0530

    Add basic framework for continuous input handling
    Temporarily uses a regular file instead of actual serial IO.

--- quadcopter_dashboard.rb
+++ quadcopter_dashboard.rb
@@ -1,6 +1,27 @@
 require 'fox16'
 include Fox
 
+class SerialIO
+  def initialize(app, frequency)
+    # Until I can get my hands on the Arduino
+    @fd = File.open('iamnotanarduino', 'r')
+    @app = app
+    @frequency = frequency
+
+    # app.addInput(@fd, INPUT_READ, method(:handleInput))
+    app.addTimeout(@frequency, method(:handleInput))
+  end
+
+  def handleInput(*args)
+    puts @fd.readline           # blocks until line is read
+    # poor man's buffer ensure's reasonably fresh data next time
+    @fd.flush
+  rescue EOFError               # probably not required for Arduino
+  ensure
+    @app.addTimeout(@frequency, method(:handleInput))
+  end
+end
+
 class DashboardWindow < FXMainWindow
   def initialize(app)
     super(app, "Quadcopter Dashboard", width: 1000, height: 700)
@@ -16,6 +37,8 @@ class DashboardWindow < FXMainWindow
                         PROGRESSBAR_DIAL | PROGRESSBAR_PERCENTAGE
                        )
     end
+
+    SerialIO.new(app, 100)
   end
 
   def create
