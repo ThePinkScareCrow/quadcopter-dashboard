84efad289bdc0559c5232425ab1b4fde76fdea49
Darshan Rai <darshanjrai@gmail.com>
2016-02-29 01:45:00 +0530

    Add padding and change window dimensions

--- quadcopter_dashboard.rb
+++ quadcopter_dashboard.rb
@@ -10,8 +10,9 @@ require_relative 'motor_gauges_matrix'
 
 class DashboardWindow < FXMainWindow
   def initialize(app)
-    super(app, "Quadcopter Dashboard", width: 1000, height: 700)
-
+    super(app, "Quadcopter Dashboard", width: 1250, height: 600)
+    self.padLeft, self.padRight = 10, 10
+    self.padTop, self.padBottom = 10, 10
     @arduino = SerialIO.new(app, self, 100)
 
     @motor_gauges_matrix = MotorGaugesMatrix.new(self, 4, MATRIX_BY_COLUMNS)
