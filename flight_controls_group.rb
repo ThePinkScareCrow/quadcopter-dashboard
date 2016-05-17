9446534fb0c3ff34141ed8035cf5d21d341496f7
Darshan Rai <darshanjrai@gmail.com>
2016-05-17 22:55:09 +0530

    Remove flight controls display of actuals

--- flight_controls_group.rb
+++ flight_controls_group.rb
@@ -5,26 +5,15 @@ class FlightControlsGroup < FXGroupBox
     super(parent, @control_name.to_s.capitalize, FRAME_RIDGE | LAYOUT_SIDE_LEFT)
     self.setFont(FXFont.new(getApp(), "Helvetica", 14, 0))
 
-    @control_actual = FXDataTarget.new(0.0)
     @control_desired = FXDataTarget.new(0.0)
 
-    flight_control_matrix = FXMatrix.new(self, 2, MATRIX_BY_COLUMNS)
-    FXLabel.new(flight_control_matrix, "Actual")
-    FXLabel.new(flight_control_matrix, "Desired")
-    FXTextField.new(flight_control_matrix, 7, @control_actual,
-                    FXDataTarget::ID_VALUE, TEXTFIELD_READONLY
-                   )
-    spinner = FXRealSpinner.new(flight_control_matrix, 7, @control_desired,
+    spinner = FXRealSpinner.new(self, 7, @control_desired,
                       FXDataTarget::ID_VALUE, FRAME_NORMAL
                                )
     spinner.setIncrement(5)
     spinner.range = -180..180
 
     @control_desired.connect(SEL_COMMAND, method(:update_desired_angle))
-  end
-
-  def update_actual_angle(angle)
-    @control_actual.value = angle
   end
 
   #######
