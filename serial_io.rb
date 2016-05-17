cb6bf660cce2b1717265aa3938ba1c1b62982d96
Darshan Rai <darshanjrai@gmail.com>
2016-05-17 22:41:20 +0530

    IO: Remove polling for telemetry data
    We do not expect any telemetry data to be sent back from the
    quadcopter. Instead it will log data on its local file system.
    
    This commit does not remove callbacks that are a part of other
    classes called when new telemetry data was received.

--- serial_io.rb
+++ serial_io.rb
@@ -1,10 +1,7 @@
 require 'serialport'
 
-NUM_VALUES_PER_LINE = 20
-
 class SerialIO
   def initialize(app, window, refresh_frequency)
-    @buffer = ""
     # The Arduino when removed and plugged in again, sometimes shows as ttyACM1
     port_str = File.exist?('/dev/ttyACM0') ? '/dev/ttyACM0' : '/dev/ttyACM1'
     baud_rate = 115200
@@ -17,19 +14,6 @@ class SerialIO
 
     @app = app
     @window = window
-    @refresh_interval = 1000 / refresh_frequency
-
-    # check for input from Arduino every @refresh_interval milliseconds
-    app.addTimeout(@refresh_interval, method(:handle_input))
-  end
-
-  def handle_input(*args)
-    string = @sp.readline
-    parse_input(string)           # blocks until line is read or EOF
-    write_unparsed(string) if @backup_file
-  rescue EOFError
-  ensure
-    @app.addTimeout(@refresh_interval, method(:handle_input))
   end
 
   # Simply sends the string it receives to the Arduino followed by a
@@ -46,33 +30,5 @@ class SerialIO
 
     def write_unparsed(string)
       @backup_file.write(string)
-    end
-
-    def parse_input(s)
-      @buffer << s
-      if (m = @buffer.match(/\^(.*?)\$/))
-        line = m[1]
-        puts line
-
-        values = line.split(' ').collect { |i| i.to_f }
-        if values.count == NUM_VALUES_PER_LINE
-          angles_actual = values[0..2]
-          angles_desired = values[3..5]
-          throttle = values[6]
-          pitch_pid = values[7..9]
-          roll_pid = values[10..12]
-          yaw_pid = values[13..15]
-          motors = values[16..19]
-
-          @window.update_values(angles_actual, angles_desired, throttle,
-                                pitch_pid, roll_pid, yaw_pid, motors
-                               )
-        end
-
-        # Clear buffer when one line has been read. This does lead to
-        # missing lines of data -- but is perfectly acceptable and
-        # ensures fresh data on next read
-        @buffer.clear
-      end
     end
 end
